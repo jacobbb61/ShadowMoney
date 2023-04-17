42ba9d943afcaf5a389aa82e151d178b24b11f31
Kim <C21519569@mytudublin.ie>
2023-04-17 16:14:16 +0100

    Witch and Tank

--- GameDesignUnity/Assets/Jacob/Scripts/GameMemory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
- 
-public class GameMemory : ScriptableObject
-{
-    [Header("Per Level")]
-    public Transform LastCheckpoint;
-}
